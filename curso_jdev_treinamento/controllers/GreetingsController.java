3a8718d2f93bb023c2317aee87698ffa4f65c3eb
Helvisklei Mendes da Silva <helvis@TMKIMIP.LOCAL>
2022-02-15 15:31:40 -0300

    Criando repository com Spring Data e fazendo a primeira inclusão teste no banco

--- curso_jdev_treinamento/controllers/GreetingsController.java
+++ curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,5 +1,6 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.curso_jdev_treinamento.model.Usuario;
+import br.com.springboot.curso_jdev_treinamento.repository.UsuarioRepository;
+
 /**
  *
  * A sample greetings controller to return greeting text
  */
 @RestController
 public class GreetingsController {
+	
+	@Autowired /*IC/CD ou CDI - Injeção de dependencia*/
+	private UsuarioRepository usuarioRepository;
     /**
      *
      * @param name the name to greet
@@ -24,11 +31,17 @@ public class GreetingsController {
         return "Curso Stpring Boot API: " + name + "!";
     }
     
+    // Os colchetes com a variavel nome informa os dados que está vindo do browser via get
     @RequestMapping(value= "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
     	
+    	// apenas um teste de inclusão no banco
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
     	
+       	usuarioRepository.save(usuario); /*para gravar no banco de dados*/
+       	
     	return "Olá mundo " + nome;    	
     }
 }
